bd0c8c8d2788e9e7abd081b7cb96885dad6671e4
soujanyac <soujanya@yellowmessenger.com>
2020-06-05 21:14:00 +0530

    added category and tag display in add new spend dialog

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:sms/sms.dart';
 import 'package:intl/intl.dart';
 import './appbarwidget.dart';
@@ -7,8 +8,23 @@ import './reminderslist.dart';
 import './accountslist.dart';
 import './tagslist.dart';
 import './add_entry_dialog.dart';
+import 'package:quiver/iterables.dart';
 
-void main() => runApp(MyApp());
+// TODO: add spends
+// TODO: read and write csv output
+// TODO: charts
+// TODO: read smses and update bank accounts
+// TODO: category basic list with icons
+// TODO:
+
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp
+  ]);
+  runApp(MyApp());
+}
 
 class MyApp extends StatefulWidget {
   @override
@@ -26,6 +42,24 @@ class _SecondRouteState extends State<SecondRoute> {
   TimeOfDay selectedTime = TimeOfDay.now();
   var newFormat = DateFormat("MMM dd, HH:mm a");
   var formatTime = DateFormat("HH:mm a");
+  var isExpense = false;
+  var shouldDisplayTagList = false;
+  var categoryList = {
+    "Bills":Icons.format_list_bulleted,
+    "EMI": Icons.graphic_eq,
+    "Entertainment": Icons.stars,
+    "Food & Drink": Icons.fastfood,
+    "Fuel": Icons.local_gas_station,
+    "Groceries": Icons.shopping_basket,
+    "Health": Icons.healing,
+    "Investment": Icons.attach_money,
+    "Shopping": Icons.shopping_cart,
+    "Transfer": Icons.transform,
+    "Travel": Icons.card_travel,
+    "Other": Icons.devices_other
+  };
+
+  var tagList = ["family", "online", "learning", "food", "clothes", "office", "friends"];
 
   Future<Null> _selectTime(BuildContext context) async {
     final picked = await showTimePicker(
@@ -49,6 +83,86 @@ class _SecondRouteState extends State<SecondRoute> {
         selectedDate = picked;
       });
     }
+  }
+
+  displayCategories(categoryList) {
+    var quads = partition(categoryList.entries, 4).toList();
+    var rowList = <Widget>[];
+    var fullRowList = <Widget>[];
+    for (var i=0; i<quads.length; i++) {
+      rowList = [];
+      for(var j=0;j<4; j++) {
+        rowList.add(
+            SizedBox(
+              height: 75,
+              width: 75,
+                child: Container(
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.only(
+                          topLeft:
+                          Radius.circular(7.0),
+                          bottomLeft:
+                          Radius.circular(7.0)),
+                      ),
+                  child: Column(
+                    children: <Widget>[
+                      Container(
+                        margin: EdgeInsets.all(7.0),
+                        decoration: BoxDecoration(
+                          shape: BoxShape.circle,
+                          color: Colors.green,
+                        ),
+                        child: Icon(quads[i][j].value),
+                        height: 40,
+                        width: 40,
+                      ),
+                      Text(quads[i][j].key, overflow: TextOverflow.ellipsis,),
+                    ],
+                  ),
+                ),
+            ),
+        );
+      }
+      fullRowList.add(
+        Row(
+          children: rowList,
+        )
+      );
+    }
+    return fullRowList;
+  }
+
+  displayTags(tagList, shouldDisplayTagList) {
+    if (!shouldDisplayTagList) {
+      return [Container()];
+    }
+    var triplets = partition(tagList, 3).toList();
+    var rowList = <Widget>[];
+    var fullRowList = <Widget>[];
+    try {
+      for (var i = 0; i < triplets.length; i++) {
+        rowList = [];
+        for (var j = 0; j < 3; j++) {
+          rowList.add(
+            Container(
+              height: 20,
+              child: FlatButton(
+                child: Text('#${triplets[i][j]}'),
+                onPressed: () {},
+              ),
+            ),
+          );
+        }
+        fullRowList.add(
+            Row(
+              children: rowList,
+            )
+        );
+      }
+    } catch (e) {
+      return fullRowList;
+    }
+    return fullRowList;
   }
 
   addSpendDialog() {
@@ -111,28 +225,63 @@ class _SecondRouteState extends State<SecondRoute> {
       ),
       ListTile(
         leading: Icon(Icons.access_time),
-        title: Text('Category'),
+        title: SizedBox(
+          child: SingleChildScrollView(
+            child: Column(
+              children: <Widget>[
+                Text('Category'),
+                ...displayCategories(categoryList),
+              ],
+            ),
+          ),
+        ),
       ),
       ListTile(
         leading: Icon(Icons.add_shopping_cart),
-        title: Text('Expense'),
-      ),
-      ListTile(
-        leading: Icon(Icons.data_usage),
-        title: Text('Note')
+        title: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: <Widget>[
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Text('Expense'),
+                Text('Will be counted in total expenses', style: TextStyle(fontSize: 12),)
+              ],
+            ),
+            Switch(
+              value: isExpense,
+              onChanged: (value) {
+                setState(() {
+                  isExpense = value;
+                });
+              },
+            )
+          ],
+        ),
       ),
       ListTile(
         leading: Icon(Icons.edit),
         title: TextField(
           decoration: InputDecoration(
+              border: InputBorder.none,
             hintText: "Add a note"
           ),
         )
       ),
       ListTile(
           leading: Icon(Icons.edit),
-          title: Text('Tag'),
-        onTap: () {},
+          title: Column(
+            children: <Widget>[
+              Text('Tag'),
+              ...displayTags(tagList, shouldDisplayTagList),
+            ],
+          ),
+        onTap: () {
+          setState(() {
+            shouldDisplayTagList = true;
+          });
+        },
       ),
       ListTile(
           leading: Icon(Icons.edit),
@@ -141,8 +290,11 @@ class _SecondRouteState extends State<SecondRoute> {
     ];
     return ListView.separated(
         itemBuilder: (context, index) {
+          var listTileHeight = 50.0;
+          if (index == 4) listTileHeight = 200.0;
+          if (index == 7) listTileHeight = 70.0;
           return Container(
-            height: 50.0,
+            height: listTileHeight,
             child: inputSpendDataList[index]
           );
         },
